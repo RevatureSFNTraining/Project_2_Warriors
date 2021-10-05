13f94ac0200a9e85918eddbc2ed30e422bc63dcd
Liam Hunt <liamhunt1789@gmail.com>
2021-10-05 11:31:59 -0400

    added discount object

--- force-app/main/default/aura/dealsAndPromos/dealsAndPromosController.js
+++ force-app/main/default/aura/dealsAndPromos/dealsAndPromosController.js
@@ -7,5 +7,9 @@
         var code = component.get("v.discountChosen.Discount_Code__c");
         console.log(code);
         component.find("textField").set("v.value", code);
+    },
+
+    addDiscount : function(component, event, helper) {
+        helper.addDiscount(component, event, helper);
     }
 })
