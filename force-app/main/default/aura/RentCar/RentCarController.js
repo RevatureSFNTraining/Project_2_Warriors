aa980f975467fa9576d708bfff00122fb9e59280
mdn117 <48142070+mdn117@users.noreply.github.com>
2021-10-05 20:37:38 -0700

    Added Car Rental Price Functionality
    also added header to components

--- force-app/main/default/aura/RentCar/RentCarController.js
+++ force-app/main/default/aura/RentCar/RentCarController.js
@@ -40,5 +40,34 @@
                                     'Expected_Return_Date__c':'', 'Status__c':'Active', 'Vehicle__c':''});
         component.set("v.newContact",{'sObjectType': 'Contact', 'FirstName': '', 'LastName': '','Birthdate': '',
                                         'Email': ''});
+    },
+    toggleSection : function(component, event, helper) {
+        // dynamically get aura:id name from 'data-auraId' attribute
+        var sectionAuraId = event.target.getAttribute("data-auraId");
+        // get section Div element using aura:id
+        var sectionDiv = component.find(sectionAuraId).getElement();
+        /* The search() method searches for 'slds-is-open' class, and returns the position of the match.
+         * This method returns -1 if no match is found.
+        */
+        var sectionState = sectionDiv.getAttribute('class').search('slds-is-open'); 
+        
+        // -1 if 'slds-is-open' class is missing...then set 'slds-is-open' class else set slds-is-close class to element
+        if(sectionState == -1){
+            sectionDiv.setAttribute('class' , 'slds-section slds-is-open cRentCar');
+
+        }else{
+            sectionDiv.setAttribute('class' , 'slds-section slds-is-close cRentCar');
+        }
+    },
+    setPrice : function(component, event, helper) {
+        var issueDate = new Date(component.get("v.newRental.Issue_Date__c"));
+        var returnDate = new Date(component.get("v.newRental.Expected_Return_Date__c"));
+        // To calculate the time difference of two dates
+        var Difference_In_Time = returnDate.getTime() - issueDate.getTime();
+        // To calculate the no. of days between two dates
+        var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+        console.log(Difference_In_Days);
+        var Price = Difference_In_Days * 25.77;
+        component.find("priceField").set("v.value", '$' + Price);
     }
 })
